2c5709c4d9f5526da67bd79c3113ec2f6c3872c2
Terauchi000 <terauchi@freemake.co.jp>
2018-09-19 15:03:16 +0900

    PHP入門　練習問題7

---
+++ KADAI php 7.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "UTF-8">
+<title>PHP入門 練習問題7</title>
+</head>
+<body>
+<from action = "送信先" method = "post">
+	名前を入力してください
+	<input type = "text" name = "name">
+	<input type ="submit" value ="ボタン"><br>
+</from><br>
+</body>
+</html>
+	
+	[送信先]
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "UTF-8">
+<title>送信先</title><br>
+</head>
+
+<body>
+「<? = $_POST['name']?>...さん、おはようございます！」
+</body>
+</html>
