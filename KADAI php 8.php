da132aa982292eab80106043446a659061154928
Terauchi000 <terauchi@freemake.co.jp>
2018-09-25 11:19:51 +0900

    PHP入門　練習問題8

---
+++ KADAI php 8.php
@@ -0,0 +1,23 @@
+﻿<?php
+	$score = 60;
+    $message = '';
+    
+	switch($score){
+	case $score >= 90: 
+	  $message = 'パーフェクトです。';
+	 break;
+	 
+	case $score >= 70 && $score < 90:
+	  $message = '素晴らしい';
+	 break;
+	 
+	case $score >= 50 && $score < 70:
+	  $message = '合格です。';
+	 break;
+	 
+	 default:
+	  $message = '頑張りましょう！';
+	 break;
+	 }
+echo "結果は $score 点です。$message";
+?>
