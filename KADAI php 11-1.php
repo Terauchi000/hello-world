45e7e033405343874224b0a05081497b2f49e21f
Terauchi000 <terauchi@freemake.co.jp>
2018-09-26 16:47:41 +0900

    PHP入門　練習問題11-1

---
+++ KADAI php 11-1.php
@@ -0,0 +1,58 @@
+﻿<?php
+	$names = array('ディープインパクト','トウカイテイオー','スペシャルウィーク','スーパークリーク','メジロマックイーン','ダイユウサク','オグリキャップ','ウォッカ','ダイワスカーレット');
+    
+     echo "出走馬は".count($names)."頭です。<br />";
+     echo "<br />";
+     	 
+	$horse[] = array(
+		'name' => 'ディープインパクト',
+		'father' => 'サンデーサイレンス'
+	    );
+	
+	$horse[] = array(
+		'name' => 'トウカイテイオー',
+		'father' => 'シンボリルドルフ'
+	    );
+	
+	$horse[] = array(
+		'name' => 'スペシャルウィーク',
+		'father' => 'サンデーサイレンス'
+	    );
+	
+	$horse[] = array(
+		'name' => 'スーパークリーク',
+		'father' => 'ノーアテンション'
+	    );
+	
+	$horse[] = array(
+		'name' => 'メジロマックイーン',
+		'father' => 'メジロティターン'
+	    );
+	
+	$horse[] = array(
+		'name' => 'ダイユウサク',
+		'father' => 'ノノアルコ'
+	    );
+	
+	$horse[] = array(
+		'name' => 'オグリキャップ',
+		'father' => 'ダンシングキャップ'
+	    );
+	
+	$horse[] = array(
+		'name' => 'ウォッカ',
+		'father' => 'タニノギムレット'
+	    );
+	
+	$horse[] = array(
+		'name' => 'ダイワスカーレット',
+		'father' => 'アグネスタキオン'
+	    );
+	//var_dump($horse);
+	
+	
+	foreach($horse as $value){
+	echo "&emsp;"."&emsp;".$value['name'].'<br>';
+	echo "&emsp;"."&emsp;"."&emsp;"."&emsp;".'父:'.$value['father'].'<br>';
+	}
+?>
